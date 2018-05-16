0a339bb787c6b15c10210c472b37865ebd8069db
Maxim Nesen <maxim.nesen@oracle.com>
2018-05-16 13:03:43 +0200

    travis buid profile

--- travis.sh
+++ travis.sh
@@ -3,4 +3,5 @@
 mvn -e -U -B clean install -Ptravis_main -fn 2>&1 | tee jersey-build.log | grep -E '(---)|(Building)|(Tests run)|(T E S T S)'
 echo '------------------------------------------------------------------------'
 tail -100 jersey-build.log
-mvn -e -U -B clean install -Ptravis_e2e
+cd tests
+mvn -e -U -B test -Ptravis_e2e
