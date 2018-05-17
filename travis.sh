2ae19521e416e09e21b26620d2b3893ec8b0fed5
Maxim Nesen <maxim.nesen@oracle.com>
2018-05-17 15:35:21 +0200

    travis buid profile

--- travis.sh
+++ travis.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
 set -ev
+echo $?
+echo $1
 mvn -e -U -B $1 2>&1 | tee jersey-build.log | grep -E '(---)|(Building)|(Tests run)|(T E S T S)'
 echo '------------------------------------------------------------------------'
 tail -100 jersey-build.log
