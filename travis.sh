1ea838398dd36c747779a68a0cc22496bec78344
Maxim Nesen <maxim.nesen@oracle.com>
2018-05-18 11:10:18 +0200

    travis buid profile

--- travis.sh
+++ travis.sh
@@ -5,7 +5,7 @@ set -ev
 set LIMIT_PARAMS="| tee jersey-build.log | grep -E '(---)|(Building)|(Tests run)|(T E S T S)'"
 set VALIDATION_PARAM="-Ptravis_e2e"
 
-if [ $1 == ${VALIDATION_PARAM} ]; then
+if [[ $1 == *"$VALIDATION_PARAM"* ]]; then
     unset $LIMIT_PARAMS
 fi
 
