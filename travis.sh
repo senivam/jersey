d001210f4bd9bb84d59cc5f892a4e9c926f34142
Maxim Nesen <maxim.nesen@oracle.com>
2018-05-18 11:04:34 +0200

    travis buid profile

--- travis.sh
+++ travis.sh
@@ -1,9 +1,15 @@
 #!/bin/bash
 
 set -ev
-echo $?
-echo $1
-mvn -e -U -B clean install $1 2>&1 | tee jersey-build.log | grep -E '(---)|(Building)|(Tests run)|(T E S T S)'
+
+set LIMIT_PARAMS="| tee jersey-build.log | grep -E '(---)|(Building)|(Tests run)|(T E S T S)'"
+set VALIDATION_PARAM="-Ptravis_e2e"
+
+if [ $1 = ${VALIDATION_PARAM} ]; then
+    unset $LIMIT_PARAMS
+fi
+
+mvn -e -U -B clean install $1 2>&1 ${LIMIT_PARAMS}
 echo '------------------------------------------------------------------------'
 tail -100 jersey-build.log
 #cd tests
