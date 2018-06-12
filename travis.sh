22f39ee34555f171058782701dc1d09d0e1582f5
Maxim Nesen <maxim.nesen@oracle.com>
2018-06-12 09:42:52 +0200

    travis buid profile

--- travis.sh
+++ travis.sh
@@ -25,7 +25,11 @@ trap 'error_handler' ERR
 bash -c "while true; do tail -5 $BUILD_OUTPUT; sleep $PING_SLEEP; done" &
 PING_LOOP_PID=$!
 
-mvn -e -U -B clean install $1 >> $BUILD_OUTPUT 2>&1
+    mvn -e -U -B clean install -Ptravis_e2e_skip >> $BUILD_OUTPUT 2>&1
+    cd tests
+    mvn -e -U -B test -Ptravis_e2e >> $BUILD_OUTPUT 2>&1
+    cd e2e-entity
+    mvn -e -U -B test -Ptravis_e2e_unstable >> $BUILD_OUTPUT 2>&1
 
 # The build finished without returning an error so dump a tail of the output
 dump_output
